5426ea938995ea3812af7fa50488f9e3d597e74f
NhiNam <nhima2611@gmail.com>
2022-03-21 20:32:18 +0700

    qweqweq

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,10 @@ import hero from '../asset/hero.jpg'
 
 const Home = () => {
     const  navigate = useNavigate();
+    const scrollTop = () => {
+      navigate( "/about");
+      window.scrollTo({top: 0, behavior: 'smooth'} )
+    }
 
   return (
     <div className='flex flex-col-reverse  h-full items-center  '>
@@ -13,7 +17,7 @@ const Home = () => {
             <h1 className='text-6xl font-semibold tracking-widest leading-tight underline decoration-2  ' >I am</h1>
             <h3 className='text-5xl leading-tight font-bold underline decoration-2 '>a Front-end Developer</h3>
             <span className='italic'>SINCE 1999</span>
-            <button onClick={() =>navigate( "/about")} className='flex justify-center items-center mt-8 gap-4 border-2 w-[200px] mx-auto  py-2 text-gray-500 hover:shadow-md mb-8'>View more <BsArrowRight/> </button>
+            <button onClick={scrollTop} className='flex justify-center items-center mt-8 gap-4 border-2 w-[200px] mx-auto  py-2 text-gray-500 hover:shadow-md mb-8'>View more <BsArrowRight/> </button>
         </div>
         <div>
             <img src={hero} alt="" />
